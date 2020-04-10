1a1f1202c716797baca91f154913c68a69d08db2
Luben Stoyanov <luben.stoyanov@yahoo.com>
2020-04-10 14:06:36 +0200

    Create skeleton for Tile

---
+++ tile.rb
@@ -0,0 +1,28 @@
+class Tile
+  def initialize
+  end
+
+  def inspect
+  end
+
+  def reveal
+  end
+
+  def neighbours
+  end
+
+  def neighbour_bomb_count
+  end
+
+  def bombed?
+  end
+
+  def flagged?
+  end
+
+  def revealed?
+  end
+
+  def safe?
+  end
+end
