d8470dd538a512b63123179ea34ec89c168c7444
Luben Stoyanov <luben.stoyanov@yahoo.com>
2020-04-10 14:42:39 +0200

    Define reveal for Tile

--- tile.rb
+++ tile.rb
@@ -1,5 +1,5 @@
 class Tile
-  def initialize(value, board, revealed, flagged=false)
+  def initialize(value, board, revealed=false, flagged=false)
     @value = value
     @board = board
     @flag = flagged
@@ -7,11 +7,11 @@ class Tile
   end
 
   def inspect
-    "#<Tile:#{object_id} @value=#{@value}, @flag=#{@flag}"
+    "#<Tile:#{object_id} @value=#{@value}, @flag=#{@flag}, @revealed=#{@revealed}>"
   end
 
   def reveal
-
+    @revealed = true
   end
 
   def neighbours
