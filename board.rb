3371b74b1a9f2df35044f5cb61026b4d1db130d3
Luben Stoyanov <luben.stoyanov@yahoo.com>
2020-04-10 13:44:52 +0200

    Define [], []=, size and place_bombs

--- board.rb
+++ board.rb
@@ -1,11 +1,32 @@
 class Board
   def initialize(n=9, bombs_count=10)
-    @grid = Array.new(n) { Array.new(n) }
+    @grid = Array.new(n) { Array.new(n, "_") }
     @bombs_count = bombs_count
   end
 
-  def place_bombs
+  def [](pos)
+    row, col = pos
+    @grid[row][col]
+  end
 
+  def []=(pos, val)
+    row, col = pos
+    @grid[row][col] = val
+  end
+
+  def place_bombs
+    count = 0
+    until count == @bombs_count
+      pos = [rand(self.size), rand(self.size)]
+      if self[pos] == "_"
+        self[pos] = "*"
+        count += 1
+      end
+    end
+  end
+
+  def size
+    @grid.length
   end
 
   def render
