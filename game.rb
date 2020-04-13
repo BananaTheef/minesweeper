f396c475c1d657622fbc210459e3dd7b4089a057
Luben Stoyanov <luben.stoyanov@yahoo.com>
2020-04-13 09:54:50 +0200

    Define prompt

--- game.rb
+++ game.rb
@@ -3,6 +3,11 @@ class Game
     @board = Board.new(n, bombs_count)
   end
 
+  def prompt
+    puts "Choose a position to be revealed or flagged(e.g.: '1,3'):"
+    puts ">"
+  end
+
   def get_pos
   end
 
