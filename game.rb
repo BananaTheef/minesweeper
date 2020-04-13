9ad6fc72d9a9e35eac7f137dc93c1d04cbc6643e
Luben Stoyanov <luben.stoyanov@yahoo.com>
2020-04-13 10:27:00 +0200

    Add @board_size and define valid_pos?

--- game.rb
+++ game.rb
@@ -1,6 +1,18 @@
+require 'byebug'
+require_relative 'board.rb'
+
 class Game
-  def initialize
+  def initialize(n=9, bombs_count=10)
     @board = Board.new(n, bombs_count)
+    @board_size = n
+  end
+
+  def valid_pos?(pos)
+    # debugger
+    return false unless pos.is_a? Array
+    return false unless pos.length == 2
+    return false unless pos.all? { |i| (0..9).include?(i) }
+    true
   end
 
   def prompt
