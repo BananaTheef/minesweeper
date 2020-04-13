22726e984845c355e4894e93b0253e5efcbf5f92
Luben Stoyanov <luben.stoyanov@yahoo.com>
2020-04-13 10:45:26 +0200

    Define get_pos and parse_int in Game

--- game.rb
+++ game.rb
@@ -1,4 +1,3 @@
-require 'byebug'
 require_relative 'board.rb'
 
 class Game
@@ -8,7 +7,6 @@ class Game
   end
 
   def valid_pos?(pos)
-    # debugger
     return false unless pos.is_a? Array
     return false unless pos.length == 2
     return false unless pos.all? { |i| (0..9).include?(i) }
@@ -21,6 +19,20 @@ class Game
   end
 
   def get_pos
+    pos = nil
+    self.prompt
+    until self.valid_pos?(pos)
+      begin
+        pos = parse_int(gets.chomp)
+      rescue
+        puts "Use digits 0-9."
+      end
+    end
+    pos
+  end
+
+  def parse_int(string)
+    string.split(",").map { |c| Integer(c) }
   end
 
   def reveal
